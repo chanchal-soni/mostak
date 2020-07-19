9f7fdbba09caa86d42852e16ae73bf80845974b6
Chanchal Soni <chanchalpsoni@gmail.com>
2020-07-19 13:54:08 +0530

    Updates

---
+++ precache-manifest.ed9b94d3513de08864bea158e46850a1.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8cc5c6bae10888a216ce13120cc9ba7",
+    "url": "/mostak/index.html"
+  },
+  {
+    "revision": "ac2fa0ae96e38cf178f1",
+    "url": "/mostak/static/css/main.2304a007.chunk.css"
+  },
+  {
+    "revision": "4fe0716c37bde8a148c0",
+    "url": "/mostak/static/js/2.4703e1fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mostak/static/js/2.4703e1fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac2fa0ae96e38cf178f1",
+    "url": "/mostak/static/js/main.0d725fe5.chunk.js"
+  },
+  {
+    "revision": "96b34c0073d47fbf0af3",
+    "url": "/mostak/static/js/runtime-main.454a15f3.js"
+  },
+  {
+    "revision": "eb51da5ca333e34f6564f43ab80c356e",
+    "url": "/mostak/static/media/bg.eb51da5c.jpg"
+  },
+  {
+    "revision": "03783c5172ee1ad128c576bf88fac168",
+    "url": "/mostak/static/media/fa-brands-400.03783c51.eot"
+  },
+  {
+    "revision": "073c2f3ce60eaf69cc2767ef3d989078",
+    "url": "/mostak/static/media/fa-brands-400.073c2f3c.svg"
+  },
+  {
+    "revision": "7559b3774a0625e8ca6c0160f8f6cfd8",
+    "url": "/mostak/static/media/fa-brands-400.7559b377.woff2"
+  },
+  {
+    "revision": "ed2b8bf117160466ba6220a8f1da54a4",
+    "url": "/mostak/static/media/fa-brands-400.ed2b8bf1.ttf"
+  },
+  {
+    "revision": "fe9d62e0d16a333a20e63c3e7595f82e",
+    "url": "/mostak/static/media/fa-brands-400.fe9d62e0.woff"
+  },
+  {
+    "revision": "59215032a4397507b80e5625dc323de3",
+    "url": "/mostak/static/media/fa-regular-400.59215032.ttf"
+  },
+  {
+    "revision": "8fdea4e89ac405d9f8db327adb331d8d",
+    "url": "/mostak/static/media/fa-regular-400.8fdea4e8.svg"
+  },
+  {
+    "revision": "e07d9e40b26048d9abe2ef966cd6e263",
+    "url": "/mostak/static/media/fa-regular-400.e07d9e40.woff2"
+  },
+  {
+    "revision": "e5770f9863963fb576942e25214a226d",
+    "url": "/mostak/static/media/fa-regular-400.e5770f98.woff"
+  },
+  {
+    "revision": "fc9c63c8224fb341fc933641cbdd12ef",
+    "url": "/mostak/static/media/fa-regular-400.fc9c63c8.eot"
+  },
+  {
+    "revision": "4bced7c4c0d61d4f988629bb8ae80b8b",
+    "url": "/mostak/static/media/fa-solid-900.4bced7c4.woff"
+  },
+  {
+    "revision": "acf50f59802f20d8b45220eaae532a1c",
+    "url": "/mostak/static/media/fa-solid-900.acf50f59.ttf"
+  },
+  {
+    "revision": "b557f56e367e59344ca95f9d1fb44352",
+    "url": "/mostak/static/media/fa-solid-900.b557f56e.svg"
+  },
+  {
+    "revision": "b5cf8ae26748570d8fb95a47f46b69e1",
+    "url": "/mostak/static/media/fa-solid-900.b5cf8ae2.woff2"
+  },
+  {
+    "revision": "ef3df98419d143d9617fe163bf4edc0b",
+    "url": "/mostak/static/media/fa-solid-900.ef3df984.eot"
+  },
+  {
+    "revision": "973279132ca2bdbaba9f38083b438f43",
+    "url": "/mostak/static/media/mostakLogo.97327913.png"
+  },
+  {
+    "revision": "63337cc73a290619340053d734f3b53e",
+    "url": "/mostak/static/media/overlay.63337cc7.png"
+  }
+]);
